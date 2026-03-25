204c33f1251217837c122458caea24d972b86023
philo1698 <pilopatesameh@gmail.com>
2026-03-25 21:22:32 +0200

    project init

---
+++ courseapp/courseapp/src/test/java/com/ph/courseapp/CourseappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ph.courseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
